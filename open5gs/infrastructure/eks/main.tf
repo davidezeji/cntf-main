2a2c4f2d70871c310b2069279ca1e4c53652d676
David Ezeji <ezejidavid@gmail.com>
2023-07-12 20:21:36 +0000

    Update main.tf

--- open5gs/infrastructure/eks/main.tf
+++ open5gs/infrastructure/eks/main.tf
@@ -273,6 +273,10 @@ resource "aws_s3_bucket" "cntf_open5gs_bucket_metrics" {
   bucket = var.bucket_name_two
 }
 
+# resource "aws_s3_bucket" "cntf-open5gs-coralogix-test-results" {
+#   bucket = var.bucket_name_three
+# }
+
 # ECR repository for custom ueransim image
 # resource "aws_ecr_repository" "cntf-ueransim-puppeteer-ecr-repository" {
 #   name                 = "var.aws_ecr_repository_name"
