a6b25d392291c143fed121515cc1d2c2cb5b6133
davidezeji <ezejidavid@gmail.com>
2023-07-24 12:17:07 -0500

    forgot to comment out a line in main.tf

--- open5gs/infrastructure/eks/main.tf
+++ open5gs/infrastructure/eks/main.tf
@@ -281,7 +281,7 @@ resource "aws_s3_bucket" "cntf_open5gs_bucket_tfstate" {
   bucket = var.bucket_name_four
 }
 
-ECR repository for custom ueransim image
+# ECR repository for custom ueransim image
 resource "aws_ecr_repository" "cntf-ueransim-puppeteer-ecr-repository" {
   name                 = "var.aws_ecr_repository_name"
   image_tag_mutability = "MUTABLE"
